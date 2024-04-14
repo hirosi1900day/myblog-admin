f723002487c2d3bbdb545472ff80090a7ceac4fe
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-04-14 12:53:05 +0900

    微修正

--- backend/migration/migration.go
+++ backend/migration/migration.go
@@ -3,10 +3,17 @@ package main
 import (
 	"gin-fleamarket/infra"
 	"gin-fleamarket/models"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	infra.Initialize()
+	// 共通化したいが実行されるファイルが異なるため、.envのパスが代わり取得に失敗するので
+	// 一旦initailize.goと同じ内容を記載
+	if err := godotenv.Load("../.env"); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	db := infra.SetupDB()
 
 	if err := db.AutoMigrate(&models.Item{}, &models.User{}); err != nil {
