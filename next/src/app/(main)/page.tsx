2c4813f1c318ff0505aa926c493f6cdcc349aaa4
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-05-02 14:56:49 +0900

    変数名がおかしい箇所をとりあえず修正

--- next/src/app/(main)/page.tsx
+++ next/src/app/(main)/page.tsx
@@ -3,8 +3,8 @@ import { TaskDocument } from '@/models/task';
 import Link from 'next/link';
 import { MdAddTask } from 'react-icons/md';
 
-const getAllTasks = async (): Promise<TaskDocument[]> => {
-  const response = await fetch(`${process.env.API_URL}/tasks`, {
+const getAllItems = async (): Promise<TaskDocument[]> => {
+  const response = await fetch(`${process.env.API_URL}/items`, {
     cache: 'no-store',
   });
   if (response.status !== 200) {
@@ -12,16 +12,16 @@ const getAllTasks = async (): Promise<TaskDocument[]> => {
   }
 
   const data = await response.json();
-  return data.tasks as TaskDocument[];
+  return data.items as TaskDocument[];
 };
 
 export default async function MainPage() {
-  const allTasks = await getAllTasks();
+  const allItems = await getAllItems();
 
   return (
     <div className="text-gray-800 p-8 h-full overflow-y-auto pb-24">
       <header className="flex justify-between items-center">
-        <h1 className="text-2xl font-bold flex items-center">All Tasks</h1>
+        <h1 className="text-2xl font-bold flex items-center">All Items</h1>
         <Link
           href="/new"
           className="flex items-center gap-1 font-semibold border px-4
@@ -32,7 +32,7 @@ export default async function MainPage() {
         </Link>
       </header>
       <div className="mt-8 flex flex-wrap gap-4">
-        {allTasks.map((task) => (
+        {allItems.map((task) => (
           <TaskCard key={task._id} task={task} />
         ))}
       </div>
