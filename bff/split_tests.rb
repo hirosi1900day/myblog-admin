6912cff372a0183bd054d934f5ba9d453c8eca96
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-06-01 09:25:12 +0900

    確認だよ〜〜

--- bff/split_tests.rb
+++ bff/split_tests.rb
@@ -3,5 +3,9 @@ all_tests = Dir["spec/**/*_spec.rb"]
 job_count = ENV["JOB_COUNT"].to_i
 job_index = ENV["JOB_INDEX"].to_i
 
+puts all_tests
+puts job_count
+puts job_index
+
 split_tests = all_tests.each_slice((all_tests.size / job_count.to_f).ceil).to_a
 split_tests[job_index] || []
