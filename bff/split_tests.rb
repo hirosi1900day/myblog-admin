428f2ccfb470ad71f826371c48da54e33edfa8ef
hirosi1900day <hirosi1900day08061700@yahoo.co.jp>
2024-06-01 12:08:47 +0900

    確認

--- bff/split_tests.rb
+++ bff/split_tests.rb
@@ -6,6 +6,7 @@ job_index = ENV["JOB_INDEX"].to_i
 puts all_tests
 puts job_count
 puts job_index
+echo "Splitting tests into #{job_count} jobs, running job #{job_index}"
 
 split_tests = all_tests.each_slice((all_tests.size / job_count.to_f).ceil).to_a
 split_tests[job_index] || []
